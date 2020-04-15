a2fff493d263afacb1c4c7dccb84ead3f2be20d3
Xiaowu Zhang <xiaowu.zhang@nexedi.com>
2020-04-15 07:49:33 +0000

    erp5_printer: add precached list

---
+++ bt5/erp5_printer/SkinTemplateItem/portal_skins/erp5_printer/WebSection_getEpsonPrecacheManifestList.py
@@ -0,0 +1,12 @@
+url_list = [
+  "renderjs.js",
+  "rsvp.js",
+  "jiodev.js",
+  "jquery.js",
+  "epos-print-4.1.0.js",
+  "erp5_epson_canvas_printout.js",
+  "erp5_epson_gadget.js",
+  "erp5_epson_printout.js"
+]
+
+return url_list
